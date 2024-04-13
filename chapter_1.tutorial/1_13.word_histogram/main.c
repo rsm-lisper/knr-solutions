9f6233d565912a233aedc6d710695ef8ad782033
Rafał -rsm- Marek <rsm@disroot.org>
2024-04-13 16:40:03 +0200

    Exerc 1_13 and 1_14

---
+++ chapter_1.tutorial/1_13.word_histogram/main.c
@@ -0,0 +1,57 @@
+
+# include <stdio.h>
+
+# define MAXLENGTH 16
+# define INWORD 1
+# define NOTINWORD 0
+
+int main ()
+{
+  int i, j, c, counters[MAXLENGTH], wordlen = 0, maxwordlen = 0, status = NOTINWORD;
+
+  for (i = 0; i < MAXLENGTH; i++) {
+    counters[i] = 0;
+  }
+
+  while ((c = getchar()) != EOF) {
+    if (c == ' ' || c == '\t' || c == '\n') {
+      if (status == INWORD) {
+	status = NOTINWORD;
+	if (wordlen > maxwordlen) {
+	  maxwordlen = wordlen;
+	}
+	if (wordlen < MAXLENGTH) {
+	  counters[wordlen]++;
+	}
+	else {
+	  counters[0]++;
+	}
+	wordlen = 0;
+      }
+    }
+    else {
+      if (status == NOTINWORD) {
+	status = INWORD;
+      }
+      wordlen++;
+    }
+  }
+
+  if (maxwordlen >= MAXLENGTH) {
+    maxwordlen = MAXLENGTH - 1;
+  }
+  for (i = 1; i <= maxwordlen; i++) {
+    printf("%3d: [%2d] ", i, counters[i]);
+    for (j = 0; j < counters[i]; j++) {
+      printf("#");
+    }
+    printf("\n");
+  }
+  if (counters[0] > 0) {
+    printf(" >%d: [%2d] ", MAXLENGTH - 1, counters[0]);
+    for (j = 0; j < counters[0]; j++) {
+      printf("#");
+    }
+    printf("\n");
+  }
+}
---
+++ chapter_1.tutorial/1_14.char_histogram/main.c
@@ -0,0 +1,31 @@
+
+# include <stdio.h>
+
+int main ()
+{
+  int c, nc[256], q, w;
+
+  for (q = 0; q < 256; q++) {
+    nc[q] = 0;
+  }
+
+  while ((c = getchar()) != EOF) {
+    nc[c]++;
+  }
+
+  printf("  Char    Count\n---------------\n");
+  for (q = 0; q < 256; q++) {
+    if (nc[q] > 0) {
+      if (q >= ' ' && q <= '~') {
+        printf("%3d [%c]: %2d ", q, q, nc[q]);
+      }
+      else {
+        printf("%3d ---: %2d ", q, nc[q]);
+      }
+      for (w = 0; w < nc[q]; w++) {
+        printf("#");
+      }
+      printf("\n");
+    }
+  }
+}
