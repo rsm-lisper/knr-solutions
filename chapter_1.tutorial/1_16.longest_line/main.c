969c67375be3ebc9bb13de300fb086d8eec7fd9a
Rafał -rsm- Marek <rsm@disroot.org>
2024-04-15 16:34:52 +0200

    Exerc 1_16.

---
+++ chapter_1.tutorial/1_16.longest_line/main.c
@@ -0,0 +1,76 @@
+
+# include <stdio.h>
+
+# define MAXLINE 12 /* max input line length */
+
+int get_line (char line[], int maxline);
+void copy (char to[], char from[]);
+
+
+/* print the longest input line */
+int main()
+{
+  int len;               /* current line length */
+  int max;               /* max length seen so far */
+  char line[MAXLINE];    /* current input line */
+  char longest[MAXLINE]; /* longest line saved here */
+
+  max = 0;
+  while ((len = get_line(line, MAXLINE)) > 0) {
+    if (len > max) {
+      max = len;
+      copy(longest, line);
+    }
+  }
+
+  if (max > 0) { /* there was a line */
+    printf("[%d]: %s", max, longest);
+    if (max > MAXLINE) {
+      printf("(...)");
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
+
+
+/* get_line: read a line into s, return length even if > lim */
+int get_line(char s[], int lim)
+{
+  int c, i;
+
+  for (i = 0; (c = getchar()) != EOF && c != '\n'; i++) {
+    if (i < lim-1) {
+      s[i] = c;
+    }
+  }
+  
+  if (i < lim-1) {
+    if (c == '\n') {
+      s[i] = c;
+      i++;
+    }
+    s[i] = '\0';
+  }
+  else {
+    if (c == '\n') {
+      i++;
+    }
+    s[lim-1] = '\0';
+  }
+  
+  return i;
+}
+
+
+/* copy: copy 'from' into 'to; assume 'to' is big enough */
+void copy (char to[], char from[])
+{
+  int i;
+  
+  i = 0;
+  while ((to[i] = from[i]) != '\0') {
+    i++;
+  }
+}
