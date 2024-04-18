69421a87ab39fb32e8c4361c2de50d5da61ffdbe
Rafał -rsm- Marek <rsm@disroot.org>
2024-04-19 00:01:21 +0200

    Exerc 1_21.

---
+++ chapter_1.tutorial/1_21.entab/main.c
@@ -0,0 +1,64 @@
+
+# include <stdio.h>
+
+# define TSIZE 8
+
+void despace (int spaces);
+
+
+int main ()
+{
+  int c, t = 0, spaces = 0;
+  
+  while ((c = getchar()) != EOF) {
+    if (spaces) {
+      if (c == ' ') {
+	spaces++;
+	t++;
+	if (t == TSIZE) {
+	  t = spaces = 0;
+	  printf("\t");
+	}
+      }
+      else {
+	if (c == '\n') {
+	  t = 0;
+	}
+	else {
+	  t = (t+1) % TSIZE;
+	}
+	despace(spaces);
+	spaces = 0;
+	printf("%c", c);
+      }
+    }
+    else { // spaces == 0
+      if (c == ' ') {
+	spaces++;
+	t++;
+	if (t == TSIZE) {
+	  t = spaces = 0;
+	  printf(" ");
+	}
+      }
+      else {
+	if (c == '\n') {
+	  t = 0;
+	}
+	else {
+	  t = (t+1) % TSIZE;
+	}
+	printf("%c", c);
+      }
+    }
+  }
+}
+
+
+void despace (int spaces)
+{
+  while (spaces > 0) {
+    spaces--;
+    printf(" ");
+  }
+}
