9ff498a9ce19dcd9f76e23f4b4dc59a4eb768203
Rafał -rsm- Marek <rsm@disroot.org>
2024-04-18 23:53:55 +0200

    Exerc 1_20.

---
+++ chapter_1.tutorial/1_20.detab/main.c
@@ -0,0 +1,29 @@
+
+# include <stdio.h>
+
+# define TSIZE 8
+
+int main ()
+{
+  int c, t;
+
+  t = 0;
+  while ((c = getchar()) != EOF) {
+    if (c == '\t') {
+      while (t < TSIZE) {
+	printf(" ");
+	t++;
+      }
+      t = 0;
+    }
+    else {
+      if (c == '\n') {
+	t = 0;
+      }
+      else {
+	t = (t + 1) % TSIZE;
+      }
+      printf("%c", c);
+    }
+  }
+}
