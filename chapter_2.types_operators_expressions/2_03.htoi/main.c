61ad371873a4934c5b941aaa65a6cb86df791a0e
Rafał -rsm- Marek <rsm@disroot.org>
2024-05-08 16:00:11 +0200

    Exerc 2_03 optimization.

--- chapter_2.types_operators_expressions/2_03.htoi/main.c
+++ chapter_2.types_operators_expressions/2_03.htoi/main.c
@@ -6,28 +6,22 @@ int con1 (int c);
 
 int main ()
 {
-  int c,
-    d = 0,
-    n = 0;
+  int c, d, number;
 
   if ((c = getchar()) == '0') {
-    if ((c = getchar()) != 'X' && c != 'x') {
-      d = con1(c);
+    if ((c = getchar()) == 'X' || c == 'x') {
+      c = getchar();
     }
   }
-  else {
-    d = con1(c);
-  }
-  if (d != -1) {
-    n = d;
-  }
-  
-  while (d != -1 && (c = getchar()) != EOF) {
+
+  d = number = 0;
+  while (d != -1 && c != EOF) {
     if ((d = con1(c)) != -1) {
-      n = n * 16 + d;
+      number = number * 16 + d;
+      c = getchar();
     }
   }
-  printf("%d\n", n);
+  printf("%d\n", number);
 
   return 0;
 }
