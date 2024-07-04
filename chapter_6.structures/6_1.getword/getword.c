599943c3ba7b61560e0abb955cd77714a0507234
Rafał -rsm- Marek <rsm@disroot.org>
2024-07-04 16:45:29 +0200

    Some cleaning up in 6_1.

--- chapter_6.structures/6_1.getword/getword.c
+++ chapter_6.structures/6_1.getword/getword.c
@@ -1,11 +1,29 @@
 # include <stdio.h>
 # include <ctype.h>
-# include "getch.h"
 
-/*
-  Our version of getword does not properly handle underscores, string
-  constants, comments, or preprocessor control lines. Write a better version.
-*/
+# define BUFSIZE 128
+
+int buf[BUFSIZE];    /* buffer for ungetch */
+int bufp = 0;        /* next free position in buf */
+
+
+/* getch: get a (possibly pushed-back) character */
+int getch (void)
+{
+  return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+
+/* ungetch: push character back on input */
+void ungetch (int c)
+{
+  if (bufp >= BUFSIZE) {
+    printf("ungetch: too many characters\n");
+  } else {
+    buf[bufp++] = c;
+  }
+}
+
 
 int ignore_nonwords ()
 {
--- chapter_6.structures/6_1.getword/getword.h
+++ chapter_6.structures/6_1.getword/getword.h
@@ -1,6 +1,11 @@
 # ifndef GETWORD_H
 # define GETWORD_H
 
+/* getch: get a (possibly pushed-back) character */
+int getch (void);
+/* ungetch: push character back on input */
+void ungetch (int c);
+
 /* getword: get next word or character from input */
 int getword (char *word, int lim);
 
