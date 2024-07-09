96e1a86f8f3b824d9593ae0f0711c744e6829448
Rafał -rsm- Marek <rsm@disroot.org>
2024-07-09 15:40:37 +0200

    Ex 6_3: improved noise words list and tests.

--- chapter_6.structures/6_3.words_lines/main.c
+++ chapter_6.structures/6_3.words_lines/main.c
@@ -7,8 +7,17 @@
 # include "binsearch.h"
 
 char *noisewords[] = {
-  "and",
-  "the"
+  "Also",  "Am",    "An",      "And",   "Are",   "As",   "At",    "Be",   "But",  "By",
+  "Did",   "Do",    "For",     "Had",   "Has",   "Have", "In",    "Is",   "Isn",  "It",
+  "Its",   "Nor",   "Not",     "Of",    "Off",   "On",   "Or",    "Out",  "So",   "Than",
+  "That",  "The",   "Then",    "There", "These", "This", "Those", "To",   "Up",   "Was",
+  "Wasn",  "When",    "With",  "Without", "Yet",
+  "also",  "am",    "an",      "and",   "are",   "as",   "at",    "be",   "but",  "by",
+  "d",     "did",   "do",      "for",   "had",   "has",  "have",  "in",   "is",   "isn",
+  "it",    "its",   "m",       "nor",   "not",   "o",    "of",    "off",  "on",   "or",
+  "out",   "p",     "re",      "s",     "so",    "t",    "than",  "that", "the",  "then",
+  "there", "these", "this",    "those", "to",    "too",  "up",    "was",  "wasn", "when",
+  "with",  "without", "yet"
 };
 
 # define NWORDS (int)(sizeof noisewords / sizeof(char *))
